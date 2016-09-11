224fa316fe977dd2db80b8b5ee4bcbae6c16c893
scoiatael <czapl.luk@gmail.com>
2016-09-11 21:24:55 +0200

    Add bootstrap script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+curl -L https://bootstrap.saltstack.com -o bootstrap_salt.sh
+# TODO: sha256sum validation
+sudo sh bootstrap_salt.sh
+
+sudo git clone https://github.com/scoiatael/zosia-server.git /srv/zosia-server
+sudo cp /srv/zosia-server/etc/salt/minion /etc/salt/minion
+sudo salt-call --local state.apply
